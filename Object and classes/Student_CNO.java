3be2103e1ec1d32a457b4c80266023d488c47350
SameepRungta <sameeprungta2002@gmail.com>
2023-10-30 22:47:53 +0530

    Student_CNO

--- Object and classes/Student_CNO.java
+++ Object and classes/Student_CNO.java
@@ -1,29 +1,101 @@
-class S_detail{
+import java.util.*;
 
-int roll_no;
-String name;
-int[] marks;
+public class Student_CNO {
 
-    public void setDetails(int roll_no, String name){
+    public static void main(String[] args) {
+    
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("Enter Roll no");
+        int roll = s.nextInt();
+        s.nextLine(); // Consume the newline left-over
+        
+        System.out.println("Enter Name");
+        String name = s.nextLine();
 
-        this.roll_no=roll_no;
-        this.name=name;
+        System.out.println("Enter Course");
+        String course = s.nextLine();
+
+        
+        System.out.println("Enter Marks for 3 subject");
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int c = s.nextInt();
+
+        student s1 = new student();
+        s1.setDetails(roll, name, course);
+            int tt=s1.total(a,b,c);
+        System.out.println("Total marks is: "+tt);
+    
+        System.out.println("Average is: "+s1.average(tt));
+            
+        s1.grade(s1.average(tt));
+
+    }
+}
+
+class student{
+
+    int roll;
+    String name;
+    String course;
+
+    int m1,m2,m3;
+
+    public void setDetails(int roll,String name,String course){
+
+        this.roll = roll;
+        this.name= name;
+        this.course = course;
 
     }
 
-public void setMarks(int[] marks){
+    public int total(int m1,int m2,int m3){
 
-    this.marks[1]=marks[];
-    this.marks[2]
+        int t= m1+m2+m3;
+        
+        return t;
 
-}
+    }
 
-}
+    public double average(int tt){
 
+        double avg=0;
+        avg= tt/3.0 ;
 
+        return avg;
 
+    }
 
+    public void grade(double avg){
 
-public class Student_CNO {
-    
+      double g= avg;  
+
+        if(g>=70){
+            System.out.println("Grade is A");
+        }
+
+        else if(g>=60 && g<70){
+            System.out.println("Grade is B");
+        }
+
+        else if(g>=50 && g<60){
+            System.out.println("Grade is C");
+        }
+
+        else if(g>=40 && g<50){
+            System.out.println("Grade is D");
+        }
+
+        else if(g<40){
+            System.out.println("Grade is F");
+        }
+
+        else {
+            System.out.println("Error");
+        }
+
+       
+    }
+
 }
