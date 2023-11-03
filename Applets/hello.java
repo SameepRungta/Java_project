2b4ca2bf759fa0a0d39b9157c4fba21cb6c2a967
SameepRungta <sameeprungta2002@gmail.com>
2023-11-03 23:31:27 +0530

    applets

---
+++ Applets/hello.java
@@ -0,0 +1,14 @@
+package Applets;
+
+import java.awt.*;
+import java.applet.*;
+
+public class hello extends Applet{
+
+    public void paint(Graphics g){
+
+        g.drawString("Hello Java",10,100);
+
+    }
+
+}
